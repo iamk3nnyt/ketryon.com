76f7ea7ea18d034e7ad29b6c443cd69839f37e2a
ktranish <ktranish@outlook.com>
2025-06-06 13:34:03 +0200

    wip

--- components/calendar.tsx
+++ components/calendar.tsx
@@ -18,7 +18,7 @@ export function Calendar() {
     <div className="rounded-xl border border-gray-200 py-4">
       <Cal
         namespace="30min"
-        calLink="ktranish/30min"
+        calLink={process.env.NEXT_PUBLIC_CAL_LINK!}
         style={{ width: "100%", height: "100%", overflow: "scroll" }}
         config={{ layout: "month_view", theme: "light" }}
       />
