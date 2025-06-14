1e3d2fd6d1cc15736fa9226b8c55d0121109b3a0
ktranish <ktranish@outlook.com>
2025-06-14 09:53:14 +0200

    wip

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -13,9 +13,9 @@ export default async function BlogArticlePage({
   const slug = (await params).slug;
 
   const article = await getArticleBySlug(slug);
-  const relatedArticles = await getRelatedArticles(slug);
-
   if (!article) return notFound();
+
+  const relatedArticles = await getRelatedArticles(slug);
 
   return (
     <main className="mx-auto flex w-full max-w-3xl flex-col gap-y-8 px-4 py-16">
